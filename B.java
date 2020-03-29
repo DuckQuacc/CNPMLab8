ab8a35fb639dd5c3aa791c4217a7f9627f8fc9ae
Victorthi <46854774+Victorthi@users.noreply.github.com>
2020-03-29 18:45:15 +0700

    Update B.java

--- B.java
+++ B.java
@@ -2,7 +2,7 @@ class Calculate{
   public static void main(String args[]){  
    System.out.println("Quick math");  
    int a;
-   a = 4987-742; 
+   a = 4983-742; 
    print("%d",a);
    print("You call this math ?");
     }  
